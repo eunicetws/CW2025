bebc706bbac71505f9e705853847de21bc353f8e
Eunice <eunicetantws@gmail.com>
2025-11-30 03:07:59 +0800

    Modify Feature - Changed randomisation technique

--- src/main/java/com/comp2042/logic/bricks/RandomBrickGenerator.java
+++ src/main/java/com/comp2042/logic/bricks/RandomBrickGenerator.java
@@ -5,33 +5,35 @@ import com.comp2042.interfaces.BrickGenerator;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomBrickGenerator implements BrickGenerator {
 
     private final List<Brick> brickList;
-
     private final Deque<Brick> nextBricks = new ArrayDeque<>();
 
     public RandomBrickGenerator() {
-        brickList = new ArrayList<>();
-        brickList.add(new IBrick());
-        brickList.add(new JBrick());
-        brickList.add(new LBrick());
-        brickList.add(new OBrick());
-        brickList.add(new SBrick());
-        brickList.add(new TBrick());
-        brickList.add(new ZBrick());
-        nextBricks.add(brickList.get(ThreadLocalRandom.current().nextInt(brickList.size())));
-        nextBricks.add(brickList.get(ThreadLocalRandom.current().nextInt(brickList.size())));
+        brickList = List.of(
+                new IBrick(), new JBrick(), new LBrick(),
+                new OBrick(), new SBrick(), new TBrick(), new ZBrick()
+        );
+
+        refillBag();
+        refillBag(); // fill 2 previews
+    }
+
+    private void refillBag() {
+        List<Brick> bag = new ArrayList<>(brickList);
+        Collections.shuffle(bag);
+        nextBricks.addAll(bag);
     }
 
     @Override
     public Brick getBrick() {
-        if (nextBricks.size() <= 1) {
-            nextBricks.add(brickList.get(ThreadLocalRandom.current().nextInt(brickList.size())));
+        if (nextBricks.size() <= 7) { // when low, refill again
+            refillBag();
         }
         return nextBricks.poll();
     }
